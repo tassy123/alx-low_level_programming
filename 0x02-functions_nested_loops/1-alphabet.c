8dd427a63b42527fc73ff83a7b25212502ae0c75
tassy123 <tasleemadedokun321@gmail.com>
2023-03-21 09:43:14 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* Print_alphabet - Make the alphabet
+* Return: Void
+*/
+
+void print_alphabet(void)
+
+{
+
+char c;
+
+for (c= 'a'; c<= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
