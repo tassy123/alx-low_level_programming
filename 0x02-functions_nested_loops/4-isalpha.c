cb6506d5c07b02e781ffa70fff94503568911d72
tassy123 <tasleemadedokun321@gmail.com>
2023-03-21 16:25:36 -0700

    alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,23 @@
 #include "main.h"
 
 /**
-* _isalpha - checks if a character is alphabetic
-* @c: the character to check
-* Return: 1 if c is alphabetic, 0 otherwise
+* main - check the code
+* Return: Always 0.
 */
 
-int _isalpha(int c)
+int main(void)
 
 {
-
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-(return 1;)
-else
-return 0;
+int r;
+r = _isalpha('H');
+_putchar(r = '0');
+r = _isalpha('o');
+_putchar(r = '0');
+r = _isalpha('100');
+_putchar(r = '0');
+r = _isalpha(':');
+_putchar(r = '0');
+_putchar('\n');
+return (0);
 
 }
