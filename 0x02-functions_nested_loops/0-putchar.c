fc47baa5b0b08739a1260a2b83a78b1b5c7bc80c
tassy123 <tasleemadedokun321@gmail.com>
2023-03-21 14:21:33 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
 * main - Entry point
@@ -9,7 +9,7 @@ int main(void)
 
 {
 
-_putchar= ('_');
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -18,6 +18,6 @@ _putchar('h');
 _putchar('a');
 _putchar('r');
 _putchar('\n');
-return (0)
+return (0);
 
 }
