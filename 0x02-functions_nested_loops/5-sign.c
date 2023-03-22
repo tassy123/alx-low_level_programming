fb79fa0c2a3dd6f0e45c4202828ee6649fd428e6
tassy123 <tasleemadedokun321@gmail.com>
2023-03-21 17:33:11 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,13 +3,15 @@
 /**
 * main - prints the sign of a number
 * Return: 1 if n is greater than zero, 0 if n is zero, -1 if n is less than zero
-* and prints the corresponding sign to the standard output
+* and print the corresponding sign to the standard output
 */
 
 int print_sign(int n)
 
 {
+
 if (n > 0)
+
 {
 _putchar('+');
 return (1);
