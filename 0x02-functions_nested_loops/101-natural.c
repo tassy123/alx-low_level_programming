a9256281c164c4bcf9fde6918467d459e3d7c522
tassy123 <tasleemadedokun321@gmail.com>
2023-03-22 10:20:02 -0700

    natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,5 +1,24 @@
-total = 0
-for i in range(1, 1024):
-if i % 3 == 0 or i % 5 == 0:
-total += i
-print(total)
+#include <stdio.h>
+
+/**
+* main - Prints the sum of all multiples of 3 or 5 upto 1024
+* Return: Always (success)
+*/
+
+int main(void)
+
+{
+int i, z = 0;
+
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+z += i;
+}
+i++;
+
+printf("%d\n", z);
+return (0);
+
+}
