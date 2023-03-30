4e00ca1964ae83c217f903659657ae06ea55b3f7
tassy123 <tasleemadedokun321@gmail.com>
2023-03-30 09:02:31 -0700

    strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* cap_string - capitalizes all words of a string.
+* @str: The string to be capitalised
+* Return: A pointer to the string changed
+*/
+
+char *cap_string(char *n)
+{
+int i = 0;
+while (n[i])
+{
+while (!(n[i] >= 'a' && n[i] <= 'z'))
+i++;
+if (n[i - 1] == ' ' ||
+n[i - 1] == '\t' ||
+n[i - 1] == '\n' ||
+n[i - 1] == ',' ||
+n[i - 1] == ';' ||
+n[i - 1] == '.' ||
+n[i - 1] == '!' ||
+n[i - 1] == '?' ||
+n[i - 1] == '"' ||
+n[i - 1] == '(' ||
+n[i - 1] == ')' ||
+n[i - 1] == '{' ||
+n[i - 1] == '}' ||
+i == 0)
+n[i] -= 32;
+i++;
+}
+return (n);
+}
