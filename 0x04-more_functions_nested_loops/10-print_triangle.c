88612192348ce8c113673405acfb5b432a84cb5c
tassy123 <tasleemadedokun321@gmail.com>
2023-03-31 02:45:42 -0700

    print

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* print_triangle - print a triangles of squares according parameter
+* @size: The size of the squares triangle
+* Retuen: empty
+*/
+
+void print_triangle(int size)
+{
+int x, y, z;
+if (size <= 0)
+{
+putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = size - x; y > 1; y--)
+{
+_putchar(32);
+}
+for (z = 0; z <= x; z++)
+{
+_putchar(35);
+}
+putchar('\n');
+}
+}
+}
--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_numbers - prints 0 to 9 
+* print_numbers - prints 0 to 9
 * Return: void.
 */
 void print_numbers(void)
