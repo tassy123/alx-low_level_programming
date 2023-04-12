f8bbe5610ea0c83a133b5998d9fd7c3248e97dbb
tassy123 <tasleemadedokun321@gmail.com>
2023-04-11 23:08:11 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,34 +1,41 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
-* str_concat - function that concatenates two strings.
-* @s1: first input
-* @s2: second input
+* str_concat - get ends of input and add together for size
+* @s1: input one to concat
+* @s2: input two to concat
 * Return: concat of s1 and s2
 */
-
 char *str_concat(char *s1, char *s2)
 {
 char *conct;
-int f, se;
+int f, er;
+
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-f = se = 0;
+
+f = er = 0;
 while (s1[f] != '\0')
 f++;
-while (s2[se] != '\0')
-se++;
-conct = malloc(sizeof(char) * (f + se + 1));
+while (s2[er] != '\0')
+er++;
+conct = malloc(sizeof(char) * (f + er + 1));
+
 if (conct == NULL)
 return (NULL);
-f = se = 0;
-while (s1[f] != '\0')
+i = ci = 0;
+while (s1[i] != '\0')
 {
-conct[f] = s2[se];
-f++, se++;
+conct[f] = s1[f];
+f++;
+}
+
+while (s2[er] != '\0')
+{
+conct[f] = s2[er];
+f++, er++;
 }
 conct[f] = '\0';
 return (conct);
