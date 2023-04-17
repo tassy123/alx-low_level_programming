6c65b89e9d090d41fc73cecc52189bc604aedaca
tassy123 <tasleemadedokun321@gmail.com>
2023-04-17 11:02:39 -0700

    init

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -14,11 +14,9 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d != NULL)
-return;
-exit(1);
 {
-d->name = name;
-d->age = age;
-d->owner = owner;
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
 }
 }
