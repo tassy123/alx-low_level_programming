250de2a86bdcba5fa5d0be06666c8caddece1896
tassy123 <tasleemadedokun321@gmail.com>
2023-04-17 11:45:29 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,6 @@
 
 /**
 * struct dog - variables of dogs
-* @d: dog to initialise
 * @name: name to initialise
 * @age: age to initialise
 * @owner: owner to initialise
@@ -14,14 +13,5 @@ struct dog
 char *name;
 float age;
 char *owner;
-};
-
-/**
-* dog_type - typedef for dog structure
-*/
-typedef struct dog dog_t;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
+}
 #endif
