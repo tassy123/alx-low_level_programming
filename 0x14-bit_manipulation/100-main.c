e2377211c0477d3d115485419d63a44433dfbde4
tassy123 <tasleemadedokun321@gmail.com>
2023-07-08 09:59:40 -0700

    100-main.c

--- 0x14-bit_manipulation/100-main.c
+++ 0x14-bit_manipulation/100-main.c
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include "main.h"
-
+/**
+* main - check the code
+* Return: Always 0.
+*/
 int main(void)
 {
-    int n;
-
-    n = get_endianness();
-    if (n != 0)
-    {
-        printf("Little Endian\n");
-    }
-    else
-    {
-        printf("Big Endian\n");
-    }
-    return (0);
+int n;
+n = get_endianness();
+if (n != 0)
+{
+printf("Little Endian\n");
+}
+else
+}
+printf("Big Endian\n");
+}
+return (0);
 }
